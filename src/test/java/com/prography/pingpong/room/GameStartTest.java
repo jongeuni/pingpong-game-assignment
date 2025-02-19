ea78c26f40fa0a497d6bf5a95c03411524770229
jongeuni <lje0818ovo@naver.com>
2025-02-19 17:57:10 +0900

    -

--- src/test/java/com/prography/pingpong/room/GameStartTest.java
+++ src/test/java/com/prography/pingpong/room/GameStartTest.java
@@ -53,11 +53,9 @@ public class GameStartTest {
 
     @Before
     public void setUp() {
-        // 1. 유저 두 명 삽입
         hostUser = userRepository.save(new User(1, 1, "name", "email", UserStatus.ACTIVE, new Date(), new Date()));
         gestUser = userRepository.save(new User(2, 2, "name", "email", UserStatus.ACTIVE, new Date(), new Date()));
 
-        // 2. 방 생성 (호스트는 첫 번째 유저)
         room = roomRepository.save(new Room("title", hostUser.getId(), RoomType.SINGLE, RoomStatusType.WAIT));
 
         userRoomRepository.save(new UserRoom(room.getId(), hostUser.getId(), "RED"));
@@ -67,7 +65,6 @@ public class GameStartTest {
 
     @Test
     public void testGameStartRoomStatusChange() throws InterruptedException {
-        // given
         doAnswer(invocation -> {
             Executors.newSingleThreadScheduledExecutor().schedule(() -> {
                 Optional<Room> updatedRoomOpt = roomRepository.findById(room.getId());
@@ -80,14 +77,11 @@ public class GameStartTest {
             return null;
         }).when(asyncGameService).scheduleRoomFinish(room.getId());
 
-        // when
         ApiResponse<Void> response = gameService.gameStart(hostUser.getId(), room.getId());
 
-        // then
-        assertEquals(200, response.getCode()); // 게임 시작 성공
+        assertEquals(200, response.getCode());
 
-        // 60초 대기 (비동기 작업 실행을 기다림)
-        Thread.sleep(61000); // 60초 + 1초의 여유
+        Thread.sleep(61000);
 
         // 방 상태가 FINISH로 변경되었는지 확인
         Room updatedRoom = roomRepository.findById(room.getId()).get();
