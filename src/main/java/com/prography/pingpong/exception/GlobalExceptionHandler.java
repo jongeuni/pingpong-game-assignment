b78f39d06711804e7a581922ed1291c06d2a9852
jongeuni <lje0818ovo@naver.com>
2025-02-21 11:56:47 +0900

    FIX 서버 에러 시 status 500 반환하도록 수정

--- src/main/java/com/prography/pingpong/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prography/pingpong/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.prography.pingpong.exception;
 import com.prography.pingpong.common.rs.ApiResponse;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<Void> handleAllExceptions(Exception ex) throws Exception {
-        return new ApiResponse<>(500);
+    public ResponseEntity<ApiResponse<Void>> handleAllExceptions(Exception ex) throws Exception {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponse<>(500));
     }
 }
